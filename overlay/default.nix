5840dedcddf202883c105faadb4aa6e1be5029c7
Julian Stecklina <js@alien8.de>
2019-11-14 00:16:08 +0100

    Added nasm mode

--- overlay/default.nix
+++ overlay/default.nix
@@ -4,6 +4,6 @@ let
   emacsWithPackages = (super.emacsPackagesNgGen super.emacs).emacsWithPackages;
 in {
   emacs = emacsWithPackages
-    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv ]));
+    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv nasm-mode ]));
 
 }
