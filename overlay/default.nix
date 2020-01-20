9773bc8918f1757b9aea857fb73434fb2299d0e8
Julian Stecklina <js@alien8.de>
2020-01-20 01:05:30 +0100

    Enable dhall mode

--- overlay/default.nix
+++ overlay/default.nix
@@ -4,6 +4,6 @@ let
   emacsWithPackages = (super.emacsPackagesNgGen super.emacs).emacsWithPackages;
 in {
   emacs = emacsWithPackages
-    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv nasm-mode ]));
+    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv nasm-mode dhall-mode ]));
 
 }
