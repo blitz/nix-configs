c1de818aedaa6512cecf7c4d23b2dade955b82ee
Julian Stecklina <js@alien8.de>
2020-01-27 18:53:30 +0100

    Enable markdown mode

--- overlay/default.nix
+++ overlay/default.nix
@@ -4,6 +4,6 @@ let
   emacsWithPackages = (super.emacsPackagesNgGen super.emacs).emacsWithPackages;
 in {
   emacs = emacsWithPackages
-    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv nasm-mode dhall-mode ]));
+    (epkgs: (with epkgs.melpaPackages; [ magit nix-mode yaml-mode direnv nasm-mode dhall-mode markdown-mode ]));
 
 }
