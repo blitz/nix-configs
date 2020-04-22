82cf062d745a869fa3fbcfed8dd1722ef6523b4e
Julian Stecklina <js@alien8.de>
2020-04-22 23:57:55 +0200

    desktop: disable autosuspend

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: {
 
+  networking.networkmanager.enable = false;
   services.openssh.enable = true;
 
+  # I use this as a build server at times.
+  services.xserver.displayManager.gdm.autoSuspend = false;
 }
