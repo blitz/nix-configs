57e26355965475777445ed00b6d53cfe9eaff7bc
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2021-10-04 13:20:20 +0200

    Drop Linux patches

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -34,29 +34,6 @@
   };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  # boot.kernelPatches = [
-  #   # {
-  #   #   name = "thinkpad-l14-temp11";
-  #   #   patch = ./thinkpad-l14-temp11.patch;
-  #   # }
-
-  #   {
-  #     name = "thinkpad-l14-temp-labels";
-  #     patch = ./thinkpad-l14-temp-labels.patch;
-  #   }
-  # ];
-
-  hardware.opengl.extraPackages = [
-    # OpenCL
-    # pkgs.rocm-opencl-runtime
-    pkgs.rocm-opencl-icd
-    # AMD's Vulkan Driver
-    # pkgs.amdvlk
-  ];
-
-  hardware.opengl.extraPackages32 = [
-    # pkgs.driversi686Linux.amdvlk
-  ];
 
   networking.networkmanager.enable = true;
 
