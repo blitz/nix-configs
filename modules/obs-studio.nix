a0c9ef43624640e5f3ca9af7dbb745ec52483fda
Julian Stecklina <js@alien8.de>
2022-01-14 17:17:26 +0100

    obs: attach v4l2loopback

--- modules/obs-studio.nix
+++ modules/obs-studio.nix
@@ -2,6 +2,9 @@
 {
   boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback video_nr=10 card_label="OBS Video Source" exclusive_caps=1
+  '';
 
   environment.systemPackages = with pkgs; [
     # OBS Studio
