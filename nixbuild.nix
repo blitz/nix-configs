079cc824da37dd04e71c8a3058f507ec41aa9b1c
Julian Stecklina <js@alien8.de>
2022-01-16 19:51:05 +0100

    nixbuild: allow aarch64 builds

--- nixbuild.nix
+++ nixbuild.nix
@@ -12,8 +12,18 @@
         maxJobs = 10;
         sshKey = "/root/.ssh/id_ed25519";
         sshUser = "root";
-        supportedFeatures = [ "big-parallel" "benchmark" ];
+        supportedFeatures = [ "big-parallel" "benchmark" "kvm" ];
         systems = [ "x86_64-linux" ];
+        speedFactor = 2;
+      }
+
+      {
+        hostName = "eu.nixbuild.net";
+        maxJobs = 10;
+        sshKey = "/root/.ssh/id_ed25519";
+        sshUser = "root";
+        supportedFeatures = [ "big-parallel" "benchmark" ];
+        systems = [ "aarch64-linux" ];
         speedFactor = 2;
       }
     ];
