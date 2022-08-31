05d718fb296897faff1ffd7d9e570018bba475aa
Julian Stecklina <js@alien8.de>
2022-08-31 20:51:42 +0300

    nixbuild: enable i686 and aarch32 remote builders

--- modules/nixbuild.nix
+++ modules/nixbuild.nix
@@ -22,10 +22,31 @@
         maxJobs = 10;
         sshKey = "/root/.ssh/id_ed25519";
         sshUser = "root";
+        supportedFeatures = [ "big-parallel" "benchmark" "kvm" "nixos-test" ];
+        systems = [ "i686-linux" ];
+        speedFactor = 2;
+      }
+
+      {
+        hostName = "eu.nixbuild.net";
+        maxJobs = 10;
+        sshKey = "/root/.ssh/id_ed25519";
+        sshUser = "root";
         supportedFeatures = [ "big-parallel" "benchmark" ];
         systems = [ "aarch64-linux" ];
         speedFactor = 2;
       }
+
+      {
+        hostName = "eu.nixbuild.net";
+        maxJobs = 10;
+        sshKey = "/root/.ssh/id_ed25519";
+        sshUser = "root";
+        supportedFeatures = [ "big-parallel" "benchmark" ];
+        systems = [ "armv7l-linux" ];
+        speedFactor = 2;
+      }
+
     ];
   };
 }
