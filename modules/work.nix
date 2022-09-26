57b0f6340f0b77a4126a5b0454a327c582664fb2
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-09-26 18:31:07 +0200

    work: add new testbox

--- modules/work.nix
+++ modules/work.nix
@@ -63,6 +63,49 @@
 
       option arch code 93 = unsigned integer 16; # RFC4578
 
+      option space ipxe;
+      option ipxe-encap-opts code 175 = encapsulate ipxe;
+      option ipxe.priority code 1 = signed integer 8;
+      option ipxe.keep-san code 8 = unsigned integer 8;
+      option ipxe.skip-san-boot code 9 = unsigned integer 8;
+      option ipxe.syslogs code 85 = string;
+      option ipxe.cert code 91 = string;
+      option ipxe.privkey code 92 = string;
+      option ipxe.crosscert code 93 = string;
+      option ipxe.no-pxedhcp code 176 = unsigned integer 8;
+      option ipxe.bus-id code 177 = string;
+      option ipxe.san-filename code 188 = string;
+      option ipxe.bios-drive code 189 = unsigned integer 8;
+      option ipxe.username code 190 = string;
+      option ipxe.password code 191 = string;
+      option ipxe.reverse-username code 192 = string;
+      option ipxe.reverse-password code 193 = string;
+      option ipxe.version code 235 = string;
+      option iscsi-initiator-iqn code 203 = string;
+      # Feature indicators
+      option ipxe.pxeext code 16 = unsigned integer 8;
+      option ipxe.iscsi code 17 = unsigned integer 8;
+      option ipxe.aoe code 18 = unsigned integer 8;
+      option ipxe.http code 19 = unsigned integer 8;
+      option ipxe.https code 20 = unsigned integer 8;
+      option ipxe.tftp code 21 = unsigned integer 8;
+      option ipxe.ftp code 22 = unsigned integer 8;
+      option ipxe.dns code 23 = unsigned integer 8;
+      option ipxe.bzimage code 24 = unsigned integer 8;
+      option ipxe.multiboot code 25 = unsigned integer 8;
+      option ipxe.slam code 26 = unsigned integer 8;
+      option ipxe.srp code 27 = unsigned integer 8;
+      option ipxe.nbi code 32 = unsigned integer 8;
+      option ipxe.pxe code 33 = unsigned integer 8;
+      option ipxe.elf code 34 = unsigned integer 8;
+      option ipxe.comboot code 35 = unsigned integer 8;
+      option ipxe.efi code 36 = unsigned integer 8;
+      option ipxe.fcoe code 37 = unsigned integer 8;
+      option ipxe.vlan code 38 = unsigned integer 8;
+      option ipxe.menu code 39 = unsigned integer 8;
+      option ipxe.sdi code 40 = unsigned integer 8;
+      option ipxe.nfs code 41 = unsigned integer 8;
+
       subnet 192.168.99.0 netmask 255.255.255.0 {
         range dynamic-bootp 192.168.99.100 192.168.99.253;
         option routers 192.168.99.1;
@@ -70,8 +113,10 @@
 
         option root-path "";
 
+        option domain-name-servers 8.8.8.8, 8.8.4.4;
+
         # Speed up DHCP if no ProxyDHCP is in the network.
-        # option ipxe.no-pxedhcp 1;
+        option ipxe.no-pxedhcp 1;
 
         class "UEFI-64-1" {
           match if substring(option vendor-class-identifier, 0, 20) = "PXEClient:Arch:00007";
@@ -99,6 +144,7 @@
         host atomnuc    { hardware ethernet b4:2e:99:bf:01:a0; fixed-address 192.168.99.6; }
         host serialnuc  { hardware ethernet f4:4d:30:6f:0a:00; fixed-address 192.168.99.7; }
         host sr650v2    { hardware ethernet 68:05:ca:e3:69:cc; fixed-address 192.168.99.8; }
+        host thinkstat  { hardware ethernet 38:f3:ab:f7:51:9d; fixed-address 192.168.99.9; }
       }
     '';
     };
@@ -125,6 +171,13 @@
           prefixLength = 24;
         }
       ];
+    };
+
+    # Offer Internet.
+    nat = {
+      enable = true;
+      externalInterface = "wlp54s0";
+      internalInterfaces = [ "enp53s0" ];
     };
   };
 }
