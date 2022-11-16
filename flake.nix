d29c6b19b3e786cf1fc500d529d1ba7dd826c6f7
Julian Stecklina <js@alien8.de>
2022-11-16 22:20:59 +0100

    all: use flake-parts

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     hercules-ci.url = "github:hercules-ci/hercules-ci-agent";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     dwarffs = {
       url = "github:edolstra/dwarffs";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,81 +24,88 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, hercules-ci, rust-overlay, dwarffs,
-              tuxedo-nixos, riff }: {
-    nixosConfigurations = {
-      canaan = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  outputs = { self, nixpkgs, nixos-hardware, hercules-ci, rust-overlay, dwarffs, tuxedo-nixos, riff, flake-parts }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      flake = {
+        nixosConfigurations = {
+          canaan = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
 
-        modules = [
-          ({ config, ... }: {
-            nixpkgs.overlays = [ rust-overlay.overlays.default ];
-            environment.systemPackages = [ riff.defaultPackage."${config.nixpkgs.system}" ];
-          })
+            modules = [
+              ({ config, ... }: {
+                nixpkgs.overlays = [ rust-overlay.overlays.default ];
+                environment.systemPackages = [ riff.defaultPackage."${config.nixpkgs.system}" ];
+              })
 
-          ./host/canaan/configuration.nix
-          ./host/canaan/hardware-configuration.nix
+              ./host/canaan/configuration.nix
+              ./host/canaan/hardware-configuration.nix
 
-          # There is a Thinkpad L14 AMD module, but it disables the
-          # IOMMU.
-          nixos-hardware.nixosModules.lenovo-thinkpad
-          nixos-hardware.nixosModules.common-pc-laptop-ssd
-          nixos-hardware.nixosModules.common-pc-laptop-acpi_call
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-gpu-amd
-        ];
+              # There is a Thinkpad L14 AMD module, but it disables the
+              # IOMMU.
+              nixos-hardware.nixosModules.lenovo-thinkpad
+              nixos-hardware.nixosModules.common-pc-laptop-ssd
+              nixos-hardware.nixosModules.common-pc-laptop-acpi_call
+              nixos-hardware.nixosModules.common-cpu-amd
+              nixos-hardware.nixosModules.common-gpu-amd
+            ];
+          };
+
+          babylon = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+
+            modules = [
+              ({ config, ... }: {
+                nixpkgs.overlays = [ rust-overlay.overlays.default ];
+                environment.systemPackages = [ riff.defaultPackage."${config.nixpkgs.system}" ];
+              })
+
+              ./host/babylon/configuration.nix
+              ./host/babylon/hardware-configuration.nix
+
+              tuxedo-nixos.nixosModules.default
+
+              nixos-hardware.nixosModules.common-pc-laptop
+              nixos-hardware.nixosModules.common-pc-laptop-ssd
+              nixos-hardware.nixosModules.common-cpu-intel
+
+              # For debugging.
+              dwarffs.nixosModules.dwarffs
+            ];
+          };
+
+          second-temple = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+
+            modules = [
+              ./host/second-temple/configuration.nix
+              ./host/second-temple/hardware-configuration.nix
+
+              nixos-hardware.nixosModules.common-pc-ssd
+              nixos-hardware.nixosModules.common-cpu-intel
+
+              hercules-ci.nixosModules.agent-service
+
+              # For debugging.
+              dwarffs.nixosModules.dwarffs
+            ];
+          };
+
+          hercules-ci-01 = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+
+            modules = [
+              ./host/hercules-ci-01/configuration.nix
+              ./host/hercules-ci-01/hardware-configuration.nix
+
+              hercules-ci.nixosModules.agent-service
+            ];
+          };
+        };
       };
-
-      babylon = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          ({ config, ... }: {
-            nixpkgs.overlays = [ rust-overlay.overlays.default ];
-            environment.systemPackages = [ riff.defaultPackage."${config.nixpkgs.system}" ];
-          })
-
-          ./host/babylon/configuration.nix
-          ./host/babylon/hardware-configuration.nix
-
-          tuxedo-nixos.nixosModules.default
-
-          nixos-hardware.nixosModules.common-pc-laptop
-          nixos-hardware.nixosModules.common-pc-laptop-ssd
-          nixos-hardware.nixosModules.common-cpu-intel
-
-          # For debugging.
-          dwarffs.nixosModules.dwarffs
-        ];
-      };
-
-      second-temple = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          ./host/second-temple/configuration.nix
-          ./host/second-temple/hardware-configuration.nix
-
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-cpu-intel
-
-          hercules-ci.nixosModules.agent-service
-
-          # For debugging.
-          dwarffs.nixosModules.dwarffs
-        ];
-      };
-
-      hercules-ci-01 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          ./host/hercules-ci-01/configuration.nix
-          ./host/hercules-ci-01/hardware-configuration.nix
-
-          hercules-ci.nixosModules.agent-service
-        ];
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, ... }: {
       };
     };
-  };
 }
