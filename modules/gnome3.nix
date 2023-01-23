367c263eed3a0abaea8aec09f4ae7202bf433f3f
Julian Stecklina <js@alien8.de>
2023-01-23 16:30:41 +0100

    gnome3: enable HP printers

--- modules/gnome3.nix
+++ modules/gnome3.nix
@@ -143,7 +143,7 @@ in
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  # services.printing.drivers = [ pkgs.brlaser ];
+  services.printing.drivers = [ pkgs.hplipWithPlugin ];
 
   # Enable sound.
   sound.enable = true;
