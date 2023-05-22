659f097beca1a63bba384442606bae9205edfc43
Julian Stecklina <js@alien8.de>
2023-05-22 21:42:34 +0200

    common: workaround some insecure dependencies

--- modules/common.nix
+++ modules/common.nix
@@ -33,6 +33,12 @@
     (self: super: { nix-direnv = super.nix-direnv.override { enableFlakes = true; }; } )
   ];
 
+  # TODO Where does this come from?
+  nixpkgs.config.permittedInsecurePackages = [
+    "nodejs-14.21.3"
+    "openssl-1.1.1t"
+  ];
+
   # Living on the edge.
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [ "mitigations=off" ];
