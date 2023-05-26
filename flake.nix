d61cc551ce5870b8e8b4de691fca4d4a49067ab5
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2023-05-26 10:20:39 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/f91ee3065de91a3531329a674a45ddcb3467a650' (2023-05-24)
      → 'github:NixOS/nixpkgs/3e01645c40b92d29f3ae76344a6d654986a91a91' (2023-05-25)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
