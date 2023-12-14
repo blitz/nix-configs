84d6f8081d9e4a953050b0ffff07b37e8329cd39
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2023-12-15 00:53:34 +0100

    plausible: forward IP information

--- modules/plausible.nix
+++ modules/plausible.nix
@@ -22,18 +22,18 @@
   services.nginx.enable = true;
 
   services.nginx.virtualHosts."${config.networking.hostName}.${config.networking.domain}" = {
-    addSSL = true;
+    forceSSL = true;
     enableACME = true;
 
     locations."/" = {
       proxyPass = "http://${config.services.plausible.server.listenAddress}:${toString config.services.plausible.server.port}";
-      proxyWebsockets = true; # needed if you need to use WebSocket
-      extraConfig =
-        # required when the target is also TLS server with multiple hosts
-        "proxy_ssl_server_name on;" +
-        # required when the server wants to use HTTP Authentication
-        "proxy_pass_header Authorization;"
-      ;
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_ssl_server_name on;
+        proxy_pass_header Authorization;
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header X-Real-IP       $remote_addr;
+      '';
     };
   };
 
