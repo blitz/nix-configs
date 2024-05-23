de2927a9e2dc2be6049e054bd16033213db153f4
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-05-23 16:39:31 +0200

    flake: use 24.05 branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
