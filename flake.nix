27182cf018f0d600f931c2907501f9f9b5c9a968
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-05-24 11:19:53 +0200

    flake: update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
@@ -23,7 +23,7 @@
     # };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
