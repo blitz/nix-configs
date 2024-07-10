5305638739a1eb040e5fe0028ad88234fdc39030
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-07-10 09:37:22 +0200

    home: autostart 1password

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,14 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  isWork = config.networking.hostName == "babylon" || config.networking.hostName == "avalon";
+in
 {
   home-manager.users.julian = { pkgs, ... }: {
 
-    home.file.".config/rustfmt/rustfmt.toml".text = ''
-      edition = "2021"
-    '';
-
-    programs = let
-      isWork = config.networking.hostName == "babylon" || config.networking.hostName == "avalon";
-    in {
+    programs = {
       ssh = {
         enable = true;
         forwardAgent = true;
@@ -105,6 +102,53 @@
         experimental-features = [ "scale-monitor-framebuffer" ];
       };
     };
+
+    # Autostart hack from:
+    # https://github.com/nix-community/home-manager/issues/3447#issuecomment-2213029759
+    home.file = let
+      autostartPrograms = [
+        # Nothing here yet
+      ]
+      ++ (lib.optional config.programs._1password-gui.enable config.programs._1password-gui.package);
+    in
+      {
+        ".config/rustfmt/rustfmt.toml".text = ''
+          edition = "2021"
+        '';
+      } //
+      builtins.listToAttrs (map
+        (pkg: {
+          name = ".config/autostart/" + pkg.pname + ".desktop";
+          value =
+            if pkg ? desktopItem
+            then {
+              # Application has a desktopItem entry.
+              # Assume that it was made with makeDesktopEntry, which exposes a
+              # text attribute with the contents of the .desktop file
+              text = pkg.desktopItem.text;
+            }
+            else {
+              # Application does *not* have a desktopItem entry. Try to find a
+              # matching .desktop name in /share/applications
+              source = with builtins; let
+                appsPath = "${pkg}/share/applications";
+                # function to filter out subdirs of /share/applications
+                filterFiles = dirContents: lib.attrsets.filterAttrs (_: fileType: elem fileType ["regular" "symlink"]) dirContents;
+              in (
+                # if there's a desktop file by the app's pname, use that
+                if (pathExists "${appsPath}/${pkg.pname}.desktop")
+                then "${appsPath}/${pkg.pname}.desktop"
+                  # if there's not, find the first desktop file in the app's directory and assume that's good enough
+                else
+                  (
+                    if pathExists "${appsPath}"
+                    then "${appsPath}/${head (attrNames (filterFiles (readDir "${appsPath}")))}"
+                    else throw "no desktop file for app ${pkg.pname}"
+                  )
+              );
+            };
+        })
+        autostartPrograms);
 
     home.stateVersion = "23.05";
   };
