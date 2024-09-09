fe0ece90b2efc20c4d8ffc4263a231eb30832d21
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-09-09 17:36:46 +0200

    avalon: style

--- host/avalon/configuration.nix
+++ host/avalon/configuration.nix
@@ -23,7 +23,8 @@
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 3;
   boot.kernelParams = [
-    "quiet" "udev.log_level=3"
+    "quiet"
+    "udev.log_level=3"
   ];
 
   boot.extraModprobeConfig = ''
