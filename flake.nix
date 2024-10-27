d00642f7c800894dc671ca5cec14f8ed023f2b86
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-10-28 00:39:55 +0100

    lix: update to 2.91.1

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
