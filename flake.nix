25bf52f8f70fb8454b2d3cc17388251efbc2d31d
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-11-16 06:25:11 +0100

    home-manager: use master until release 24.11 is available

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
