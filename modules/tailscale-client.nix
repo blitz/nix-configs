66d86073034fb060e219e1609805147fba8960d7
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-09-01 18:41:51 +0200

    tailscale: workaround kernel regression

--- modules/tailscale-client.nix
+++ modules/tailscale-client.nix
@@ -4,4 +4,23 @@
     enable = true;
     useRoutingFeatures = "client";
   };
+
+  # https://github.com/tailscale/tailscale/issues/16966
+  # https://github.com/NixOS/nixpkgs/issues/438765
+  #
+  # Should be gone after a kernel update.
+  nixpkgs.overlays = [
+    (_: prev: {
+      tailscale = prev.tailscale.overrideAttrs (old: {
+        checkFlags =
+          builtins.map (
+            flag:
+            if prev.lib.hasPrefix "-skip=" flag
+            then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+            else flag
+          )
+            old.checkFlags;
+      });
+    })
+  ];
 }
