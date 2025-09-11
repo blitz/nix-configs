051a215a969ca29427bf04767fe00d616f90e569
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-09-11 23:47:17 +0200

    first-temple: add tailscale workaround

--- modules/tailscale-exit-node.nix
+++ modules/tailscale-exit-node.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
+
+    # https://github.com/tailscale/tailscale/issues/16966
+    # https://github.com/NixOS/nixpkgs/issues/438765
+    #
+    # Should be gone after a kernel update.
+    package = pkgs.tailscale.overrideAttrs { doCheck = false; };
   };
 }
