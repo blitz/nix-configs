3217a72b969c7ce11aadd8969f39f0a3b9dd135d
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-09-15 00:08:53 +0200

    tailscale: remove hacks

--- modules/tailscale-client.nix
+++ modules/tailscale-client.nix
@@ -3,11 +3,5 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
-
-    # https://github.com/tailscale/tailscale/issues/16966
-    # https://github.com/NixOS/nixpkgs/issues/438765
-    #
-    # Should be gone after a kernel update.
-    package = pkgs.tailscale.overrideAttrs { doCheck = false; };
   };
 }
