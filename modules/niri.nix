de9a01f4f4be341e7e30eb0d36012efb35a5e3fb
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-11-10 10:41:16 +0100

    niri: add more common tools

--- modules/niri.nix
+++ modules/niri.nix
@@ -3,7 +3,19 @@
 
   # These are used by default in Niri.
   environment.systemPackages = with pkgs; [
+    # Used by the default config.
     fuzzel
     alacritty
+
+    # Notifications
+    mako
+
+    # Lock screen/idle
+    swaylock
+    swayidle
+
+    # To get a useful tray bar.
+    waybar
+    pavucontrol
   ];
 }
