808fc03181e24bfd21f1386d058cae1363ce5d04
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-01-19 09:28:53 +0100

    plausible: calm down

--- modules/plausible.nix
+++ modules/plausible.nix
@@ -10,6 +10,40 @@
     };
   };
 
+  # https://github.com/NixOS/nixpkgs/issues/196935#issuecomment-1767451038
+  environment.etc = {
+    "clickhouse-server/users.d/disable-logging-query.xml" = {
+      text = ''
+        <clickhouse>
+          <profiles>
+            <default>
+              <log_queries>0</log_queries>
+              <log_query_threads>0</log_query_threads>
+            </default>
+          </profiles>
+        </clickhouse>
+      '';
+    };
+    "clickhouse-server/config.d/reduce-logging.xml" = {
+      text = ''
+        <clickhouse>
+          <logger>
+            <level>warning</level>
+            <console>true</console>
+          </logger>
+          <query_thread_log remove="remove"/>
+          <query_log remove="remove"/>
+          <text_log remove="remove"/>
+          <trace_log remove="remove"/>
+          <metric_log remove="remove"/>
+          <asynchronous_metric_log remove="remove"/>
+          <session_log remove="remove"/>
+          <part_log remove="remove"/>
+        </clickhouse>
+      '';
+    };
+  };
+
   # This should probably use LoadCredential.
   systemd.services.plausible.serviceConfig.EnvironmentFile = "/var/lib/plausible-google-search";
 
