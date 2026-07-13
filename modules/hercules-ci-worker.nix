50302448ddafdc609b0b517af33aa1550b040ca5
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-07-13 09:39:20 +0200

    hercules-ci: run on celler-cache org

--- modules/hercules-ci-worker.nix
+++ modules/hercules-ci-worker.nix
@@ -16,12 +16,29 @@
     owner = "hci-blitz";
   };
 
+  age.secrets.celler-cluster-join-token = {
+    file = ../secrets/celler-cluster-join-token.key.age;
+    owner = "hci-celler";
+  };
+
+  age.secrets.celler-binary-caches = {
+    file = ../secrets/celler-binary-caches.json.age;
+    owner = "hci-celler";
+  };
+
   services.hercules-ci-agents = {
     blitz = {
       settings.concurrentTasks = 2;
 
       settings.clusterJoinTokenPath = config.age.secrets.blitz-cluster-join-token.path;
       settings.binaryCachesPath = config.age.secrets.blitz-binary-caches.path;
+    };
+
+    celler = {
+      settings.concurrentTasks = 2;
+
+      settings.clusterJoinTokenPath = config.age.secrets.celler-cluster-join-token.path;
+      settings.binaryCachesPath = config.age.secrets.celler-binary-caches.path;
     };
   };
 
