b5f0636d3155f373b16b20a629b46478ea0f6705
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-08-25 11:38:59 +0200

    amdgpu: enable hw encoding and rocm

--- modules/amdgpu.nix
+++ modules/amdgpu.nix
@@ -5,29 +5,58 @@
   ...
 }:
 
+let
+  cfg = config.blitz.amdgpu;
+in
 {
-  boot.kernelParams = [
-    # May help with AMDGPU gfxhub page faults.
-    #
-    # TODO: We have a discrete GPU now. Let's see!
-    # "amdgpu.sg_display=0"
-  ];
+  options.blitz.amdgpu = {
+    hwVideoDecoding = lib.mkEnableOption "hardware video decoding" // {
+      default = true;
+    };
 
-  hardware.amdgpu = {
-    #initrd.enable = true;
-    opencl.enable = true;
+    hwVideoEncoding = lib.mkEnableOption "hardware video encoding";
   };
 
-  services.kmscon.enable = true;
+  config = {
+    boot.kernelParams = [
+      # May help with AMDGPU gfxhub page faults.
+      #
+      # TODO: We have a discrete GPU now. Let's see!
+      # "amdgpu.sg_display=0"
+    ];
 
-  boot.kernelPatches = [
-    {
-      name = "amdgpu";
-      patch = null;
+    nixpkgs.overlays = [
+      (final: prev: {
+        google-chrome = prev.google-chrome.override {
+          commandLineArgs =
+            [ ]
+            ++ (lib.optional cfg.hwVideoDecoding "--enable-features=VaapiVideoDecoder")
+            ++ (lib.optional cfg.hwVideoEncoding "--enable-features=VaapiVideoEncoder");
+        };
+      })
+    ];
 
-      structuredExtraConfig = with lib.kernel; {
-        DRM_AMDGPU = yes;
-      };
-    }
-  ];
+    nixpkgs.config.rocmSupport = true;
+
+    hardware.amdgpu = {
+      #
+      #initrd.enable = true;
+
+      opencl.enable = true;
+    };
+
+    services.kmscon.enable = true;
+    services.lact.enable = true;
+
+    boot.kernelPatches = [
+      {
+        name = "amdgpu";
+        patch = null;
+
+        structuredExtraConfig = with lib.kernel; {
+          DRM_AMDGPU = yes;
+        };
+      }
+    ];
+  };
 }
