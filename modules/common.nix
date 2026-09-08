5623eaab867b176ed7268e9e6b10c63469f00ef9
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-09-08 09:26:42 +0200

    common: fix systemd config

--- modules/common.nix
+++ modules/common.nix
@@ -88,10 +88,22 @@ in
 
     # Don't accumulate crap.
     boot.tmp.cleanOnBoot = true;
-    services.journald.extraConfig = ''
-      SystemMaxUse=250M
-      SystemMaxFileSize=50M
-    '';
+
+    services.journald =
+      if lib.versionOlder lib.version "26.11" then
+        {
+          extraConfig = ''
+            SystemMaxUse=250M
+            SystemMaxFileSize=50M
+          '';
+        }
+      else
+        {
+          settings.Journal = {
+            SystemMaxUse = "250M";
+            SystemMaxFileSize = "50M";
+          };
+        };
 
     nix.optimise.automatic = true;
     boot.loader.systemd-boot.configurationLimit = 5;
